93a5f7d0c42dac65caba185d59da27cf949659c9
Alden Dent <55511021+adent11@users.noreply.github.com>
2021-11-20 03:34:56 +0000

    Change to storing whether each position can be moved to in a dict rather than many lists of unavailable positions for speed

--- closestFirst.py
+++ closestFirst.py
@@ -4,7 +4,7 @@ from customDataTypes import PriorityQueue
 
 PER_ROW = int(input("Enter an odd width:  "))
 PER_COLUMN = int(input("Enter an odd height:  "))
-SQUARE_SIZE = 10
+SQUARE_SIZE = 5
 WIDTH, HEIGHT = SQUARE_SIZE * PER_ROW, SQUARE_SIZE * PER_COLUMN
 BLOCK_SIZE_OFFSET = 0
 BLOCK_SIZE = SQUARE_SIZE - 2 * BLOCK_SIZE_OFFSET
@@ -17,7 +17,7 @@ BLACK = (0, 0, 0)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Closest First Search Maze Solving")
 clock = pygame.time.Clock()  # Starts a clock
-FRAME_LENGTH = 10
+FRAME_LENGTH = 1
 
 possibleMoves = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 
@@ -31,23 +31,6 @@ def waitFor(t):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-
-def waitForSpace():
-    spacePressed = False
-    while not spacePressed:
-        keys_pressed = pygame.key.get_pressed()  # Gets the keys pressed
-        if keys_pressed[pygame.K_ESCAPE]:  # Quits if the escape key is pressed
-            pygame.quit()
-            exit()
-        for event in pygame.event.get():  # Quits if window is closed
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    spacePressed = True
-                if event.key == pygame.K_p:
-                    print(parentNode)
 
 def drawNode(node, color):
     pygame.draw.rect(
@@ -66,42 +49,44 @@ def inBounds(pos):
     else:
         return False
 
-def nextPositions(pos, searchedPos, nodeQueue, maze):
+def nextPositions(pos, isOpenPos):
     nextPositions = []
     for move in possibleMoves:
         newPos = (pos[0] + move[0], pos[1] + move[1])
-        if inBounds(newPos) and not newPos in set(searchedPos) and maze[newPos[0]][newPos[1]] != 1 and newPos not in set(nodeQueue.allData):
+        if isOpenPos[newPos]:
             nextPositions = nextPositions + [(pos[0] + move[0], pos[1] + move[1])]
     return nextPositions
 
 def solution(maze):
+    isOpenPos = {(y, x):True for y in range(-1, PER_COLUMN + 1) for x in range(-1, PER_ROW + 1)}
     WIN.fill(WHITE)
-    for y in range(len(maze)):
-        for x in range(len(maze[0])):
-            if maze[y][x] == 1:
+    for y in range(-1, PER_COLUMN + 1):
+        for x in range(-1, PER_ROW + 1):
+            if y == -1 or y == PER_COLUMN or x == -1 or x == PER_ROW:
+                isOpenPos[(y, x)] = False
+            elif maze[y][x] == 1:
                 drawNode((y, x), BLACK)
-                waitFor(pygame.time.get_ticks()+1)
+                isOpenPos[(y, x)] = False
+                #waitFor(pygame.time.get_ticks()+1)
     nodeQueue = PriorityQueue()
     startPos = (0, 0)
+    isOpenPos[startPos] = False
     endPos = (PER_COLUMN-1, PER_ROW-1)
-    searchedNodes = []
     parentNode = {}
     nodeQueue.insert(startPos, getDistance(startPos, endPos))
     drawNode(startPos, ALIVE_COLOR)
     waitFor(pygame.time.get_ticks()+FRAME_LENGTH)
-    while endPos not in searchedNodes and len(nodeQueue.items) > 0:
-        print(endPos in parentNode)
+    while isOpenPos[endPos] and len(nodeQueue.items) > 0:
         nextFrameTime = pygame.time.get_ticks() + FRAME_LENGTH
         curPos = nodeQueue.popLowest()
         drawNode(curPos, DEAD_COLOR)
-        for pos in nextPositions(curPos, searchedNodes, nodeQueue, maze):
+        for pos in nextPositions(curPos, isOpenPos):
             nodeQueue.insert(pos, getDistance(pos, endPos))
             drawNode(pos, ALIVE_COLOR)
             parentNode[pos] = curPos
-        searchedNodes.append(curPos)
+            isOpenPos[pos] = False
         waitFor(nextFrameTime)
-        #waitForSpace()
-        #print(f"Latency: {pygame.time.get_ticks()-(nextFrameTime)}")
+        print(f"Latency: {pygame.time.get_ticks()-(nextFrameTime)}")
     
     curNode = endPos
     while curNode in parentNode:
@@ -110,6 +95,6 @@ def solution(maze):
         drawNode(parentNode[curNode], (0, 0, 255))
         curNode = parentNode[curNode]
 
-while True:  # Delays (waits for user to exit) infinitely
+while True:  # Infinitely generates a random maze and solves it then delays 5 seconds
     solution(kruskalsMazeCreator.generateMaze(PER_COLUMN, PER_ROW))
     waitFor(pygame.time.get_ticks() + 5000)
